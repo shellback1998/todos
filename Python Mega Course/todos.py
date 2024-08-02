712d943cd680ab74da4da268169305864a99114e
Scott <shellback1998@gmail.com>
2024-08-02 09:02:45 -0400

    Fixed a bug for the Show command

--- Python Mega Course/todos.py
+++ Python Mega Course/todos.py
@@ -8,7 +8,7 @@ while True:
         with open('todofiles/todos.txt','r') as file:
             todos = file.readlines()
 
-        todos.append(todo)
+        todos.append(todo + "\n")
 
         with open('todofiles/todos.txt', 'w') as file:
             file.writelines(todos)
