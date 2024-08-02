05cd3e1e9ff6700cac5e950c0b7db1de3525d9e5
Scott <shellback1998@gmail.com>
2024-08-02 12:17:27 -0400

    Added custom function and got rid of some reused code for the open file read statements

--- Python Mega Course/todos.py
+++ Python Mega Course/todos.py
@@ -1,3 +1,9 @@
+def get_todos():
+    with open('todofiles/todos.txt', 'r') as file_local:
+        todos_local = file_local.readlines()
+    return todos_local
+
+
 while True:
     user_action = input("Type add, show, edit, complete or exit: ")
     user_action = user_action.strip().lower()
@@ -5,8 +11,7 @@ while True:
     if user_action.startswith("add"):
         todo = user_action[4:]
 
-        with open('todofiles/todos.txt','r') as file:
-            todos = file.readlines()
+        todos = get_todos()
 
         todos.append(todo + "\n")
 
@@ -15,8 +20,7 @@ while True:
 
     elif user_action.startswith("show"):
 
-        with open('todofiles/todos.txt', 'r') as f:
-            todos = f.readlines()
+        todos = get_todos()
 
         for index, item in enumerate(todos):
             item = item.strip('\n')
@@ -28,8 +32,7 @@ while True:
             number = int(user_action[5:])
             number = number - 1
 
-            with open('todofiles/todos.txt', 'r') as f:
-                todos = f.readlines()
+            todos = get_todos()
 
             new_todo = input("Enter new todo: ")
             todos[number] = new_todo + "\n"
@@ -45,8 +48,8 @@ while True:
             number = int(user_action[9:])
             todos.pop(number - 1)
 
-            with open('todofiles/todos.txt', 'r') as f:
-                todos = f.readlines()
+            todos = get_todos()
+
             index = number - 1
             todo_to_remove = todos[index].strip('\n')
             todos.pop(index)
