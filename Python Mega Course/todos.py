eb4b871e8bb42f7544791acb52062ca1f75a7d1e
Scott <shellback1998@gmail.com>
2024-08-02 13:16:21 -0400

    Added custom function fpr the write function to the todos.txt

--- Python Mega Course/todos.py
+++ Python Mega Course/todos.py
@@ -1,7 +1,12 @@
-def get_todos():
-    with open('todofiles/todos.txt', 'r') as file_local:
+def get_todos(filepath):
+    with open(filepath, 'r') as file_local:
         todos_local = file_local.readlines()
     return todos_local
+
+
+def write_todos(filepath_arg, todos_arg):
+    with open(filepath_arg, 'w') as file_local:
+        file_local.writelines(todos_arg)
 
 
 while True:
@@ -11,16 +16,15 @@ while True:
     if user_action.startswith("add"):
         todo = user_action[4:]
 
-        todos = get_todos()
+        todos = get_todos("todofiles/todos.txt")
 
         todos.append(todo + "\n")
 
-        with open('todofiles/todos.txt', 'w') as file:
-            file.writelines(todos)
+        write_todos("todofiles/todos.txt", todos)
 
     elif user_action.startswith("show"):
 
-        todos = get_todos()
+        todos = get_todos("todofiles/todos.txt")
 
         for index, item in enumerate(todos):
             item = item.strip('\n')
@@ -32,13 +36,13 @@ while True:
             number = int(user_action[5:])
             number = number - 1
 
-            todos = get_todos()
+            todos = get_todos("todofiles/todos.txt")
 
             new_todo = input("Enter new todo: ")
             todos[number] = new_todo + "\n"
 
-            with open('todofiles/todos.txt', 'w') as f:
-                f.writelines(todos)
+            write_todos("todofiles/todos.txt", todos)
+
         except ValueError:
             print("Your command is not valid")
             continue
@@ -48,14 +52,13 @@ while True:
             number = int(user_action[9:])
             todos.pop(number - 1)
 
-            todos = get_todos()
+            todos = get_todos("todofiles/todos.txt")
 
             index = number - 1
             todo_to_remove = todos[index].strip('\n')
             todos.pop(index)
 
-            with open('todofiles/todos.txt', 'w') as f:
-                f.writelines(todos)
+            write_todos("todofiles/todos.txt", todos)
 
             message = f"Todo {todo_to_remove} was removed from the list"
             print(message)
