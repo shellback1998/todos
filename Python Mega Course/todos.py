0f9907648ce7acb27a67e428f4fed6e62e9c87e3
Scott <shellback1998@gmail.com>
2024-08-03 13:57:33 -0400

    Added doc strings for the get_todos function.

--- Python Mega Course/todos.py
+++ Python Mega Course/todos.py
@@ -1,10 +1,14 @@
 def get_todos(filepath="todofiles/todos.txt"):
+    """ Read a text file and return the list of
+    to-do itesms.
+    """
     with open(filepath, 'r') as file_local:
         todos_local = file_local.readlines()
     return todos_local
 
 
 def write_todos(todos_arg, filepath_arg="todofiles/todos.txt" ):
+    """ Write the to-do items list in the text file."""
     with open(filepath_arg, 'w') as file_local:
         file_local.writelines(todos_arg)
 
