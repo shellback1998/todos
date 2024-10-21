d48448ecbd94e7f1e08994531aeac89d99f12eb9
Scott Gaudet <scott.gaudet@ge.com>
2024-10-21 08:20:32 -0400

    Clean up and added a gitignore

--- bonus/files/test.py
+++ bonus/files/test.py
@@ -5,17 +5,17 @@ while True:
     if 'add' in user_action:
         todo = user_action[4:]
         print("You typed add")
-        with open('todofiles/todos.txt') as file:
+        with open('../../Python Mega Course/todofiles/todos.txt') as file:
             todos = file.readlines()
             print("you are within the with statement")
         todos.append(todo)
         print("just added the todo")
-        with open('todofiles/todos.txt', 'w') as file:
+        with open('../../Python Mega Course/todofiles/todos.txt', 'w') as file:
             file.writelines(todos)
 
     elif 'show' in user_action:
 
-        with open('todofiles/todos.txt', 'r') as f:
+        with open('../../Python Mega Course/todofiles/todos.txt', 'r') as f:
             todos = f.readlines()
 
         for index, item in enumerate(todos):
@@ -27,26 +27,26 @@ while True:
         number = int(input("Number of the todo to edit: "))
         number = number - 1
 
-        with open('todofiles/todos.txt', 'r') as f:
+        with open('../../Python Mega Course/todofiles/todos.txt', 'r') as f:
             todos = f.readlines()
 
         new_todo = input("Enter new todo: ")
         todos[number] = new_todo + "\n"
 
-        with open('todofiles/todos.txt', 'w') as f:
+        with open('../../Python Mega Course/todofiles/todos.txt', 'w') as f:
             f.writelines(todos)
 
     elif 'complete' in user_action:
         number = int(input("Number of the todo to complete: "))
         todos.pop(number - 1)
 
-        with open('todofiles/todos.txt', 'r') as f:
+        with open('../../Python Mega Course/todofiles/todos.txt', 'r') as f:
             todos = f.readlines()
         index = number - 1
         todo_to_remove = todos[index].strip('\n')
         todos.pop(index)
 
-        with open('todofiles/todos.txt', 'w') as f:
+        with open('../../Python Mega Course/todofiles/todos.txt', 'w') as f:
             f.writelines(todos)
 
         message = f"Todo {todo_to_remove} was removed from the list"
