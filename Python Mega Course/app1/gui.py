e91a14b415b8f32b1688d1f50d94b932b649162b
Scott Gaudet <scott.gaudet@ge.com>
2024-10-23 10:17:36 -0400

    Fixed an error. It was calling the old verson of the software

--- Python Mega Course/app1/gui.py
+++ Python Mega Course/app1/gui.py
@@ -3,14 +3,14 @@ from ipaddress import ip_network
 import functions
 import FreeSimpleGUI as sg
 
-from bonus.files.test import index
-
 label = sg.Text("Type in a to-do")
 input_box = sg.InputText(tooltip="Enter todo", key="todo")
 add_button = sg.Button("Add")
 list_box = sg.Listbox(values=functions.get_todos(), key="todos",
-                      enable_events=True, size=[45, 10])
+                      enable_events=True, size=(45, 10))
 edit_button = sg.Button("Edit")
+
+layout = [[label], [input_box, add_button],[list_box, edit_button]]
 
 window = sg.Window('My To-Do App',
                    layout=[[label], [input_box, add_button],[list_box, edit_button]],
