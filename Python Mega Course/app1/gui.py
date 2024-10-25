f285ed6aa26eb70fdc4b92bea2277f56656428e4
Scott Gaudet <scott.gaudet@ge.com>
2024-10-25 08:37:35 -0400

    Implement the complete button as well as the exit button.

--- Python Mega Course/app1/gui.py
+++ Python Mega Course/app1/gui.py
@@ -9,11 +9,16 @@ add_button = sg.Button("Add")
 list_box = sg.Listbox(values=functions.get_todos(), key="todos",
                       enable_events=True, size=(45, 10))
 edit_button = sg.Button("Edit")
+complete_button = sg.Button("Complete")
+exit_button = sg.Button("Exit")
 
 layout = [[label], [input_box, add_button],[list_box, edit_button]]
 
 window = sg.Window('My To-Do App',
-                   layout=[[label], [input_box, add_button],[list_box, edit_button]],
+                   layout=[[label] ,
+                           [input_box, add_button],
+                           [list_box, edit_button, complete_button],
+                           [exit_button]],
                    font=('Helvetica', 20))
 
 while True:
@@ -35,10 +40,19 @@ while True:
             todos = functions.get_todos()
             todos.index(todo_to_edit)
             todos[index] = new_todo
-            functions.write_todos(index)
+            functions.write_todos(todos)
             window['todos'].update(values=todos)
+        case 'Complete':
+            todo_to_complete = values['todos'][0]
+            todos = functions.get_todos()
+            todos.remove(todo_to_complete)
+            functions.write_todos(todos)
+            window['todos'].update(values=todos)
+            window['todo'].update(value='')
         case 'todos':
             window['todo'].update(value=values['todos'][0])
+        case 'Exit':
+            break
         case sg.WINDOW_CLOSED:
             break
 
