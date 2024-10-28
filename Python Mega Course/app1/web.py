adb226f26d32ca486bb3da7b41b1586067420b37
Scott Gaudet <scott.gaudet@ge.com>
2024-10-28 08:36:25 -0400

    Fixed errors

--- Python Mega Course/app1/web.py
+++ Python Mega Course/app1/web.py
@@ -5,7 +5,7 @@ import  functions
 todos = functions.get_todos()
 
 def add_todo():
-    new_todo = st.session_state["new_todo"] + "\n"
+    todo = st.session_state["new_todo"] + "\n"
     todos.append(todo)
     functions.write_todos(todos)
 
@@ -18,10 +18,15 @@ st.write("This app is to increase your productivity.")
 st.checkbox("Buy Grocery")
 
 
-for todo in todos:
-    st.checkbox(todo)
+for index, todo in enumerate(todos):
+    checkbox = st.checkbox(todo, key=todo)
+    if checkbox:
+        todos.pop(index)
+        functions.write_todos(todos)
+        del st.session_state[todo]
+        st.rerun()
 
 st.text_input(label="", placeholder="Add a new todo",
-              on_change="add_todo",key='new_todo')
+              on_change=add_todo,key='new_todo')
 
 st.session_state
