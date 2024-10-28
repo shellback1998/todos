19a3ee5d1ea4745576e2544af84982d703bc1d49
Scott Gaudet <scott.gaudet@ge.com>
2024-10-28 07:46:44 -0400

    IMplement "Add new todo" feature

--- Python Mega Course/app1/web.py
+++ Python Mega Course/app1/web.py
@@ -3,6 +3,13 @@ import  functions
 
 
 todos = functions.get_todos()
+
+def add_todo():
+    new_todo = st.session_state["new_todo"] + "\n"
+    todos.append(todo)
+    functions.write_todos(todos)
+
+todos = functions.get_todos()
 st.title("My Todo App")
 st.subheader("This is my todo app")
 st.write("This app is to increase your productivity.")
@@ -14,4 +21,7 @@ st.checkbox("Buy Grocery")
 for todo in todos:
     st.checkbox(todo)
 
-st.text_input(label="", placeholder="Add a new todo")
+st.text_input(label="", placeholder="Add a new todo",
+              on_change="add_todo",key='new_todo')
+
+st.session_state
